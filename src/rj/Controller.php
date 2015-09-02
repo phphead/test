68454270da9f2a693c2173c88d5b8739e9b10345
i.korneev <i.korneev@dwteam.ru>
2015-09-02 15:31:19 +0300

    test init

--- src/rj/Controller.php
+++ src/rj/Controller.php
@@ -1,5 +1,18 @@
 <?php namespace Rj;
 
+use Exception;
+
 class Controller extends \Phalcon\Mvc\Controller {
+
+	public function assertNoMessages(\Phalcon\Mvc\Model $model) {
+		$messages = $model->getMessages();
+		if (count($messages)) {
+			$result = '';
+			foreach ($messages as $message) {
+				$result .= $message->getMessage() . "\n";
+			}
+			throw new Exception($result);
+		}
+	}
 
 }
