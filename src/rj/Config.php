70e0634917246591ebf921635da01b8f0ba58418
i.korneev <i.korneev@dwteam.ru>
2015-09-02 14:59:54 +0300

    test init

--- src/rj/Config.php
+++ src/rj/Config.php
@@ -5,6 +5,12 @@ class Config {
 	/** @var bool */
 	public $mail_exceptions;
 
+	/** @var string */
+	public $display_errors;
+
+	/** @var int */
+	public $error_reporting;
+
 	/** @return \Config */
 	public static function instance() {
 		return \Phalcon\DI::getDefault()->getShared('config');
