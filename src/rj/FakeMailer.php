899f7e096c212eaf4036a4745ad13fabced6fcfd
i.korneev <i.korneev@dwteam.ru>
2015-09-03 17:37:53 +0300

    ok

---
+++ src/rj/FakeMailer.php
@@ -0,0 +1,31 @@
+<?php namespace Rj;
+
+class MailerLog
+{
+    public static $_logger = 'email';
+
+    public $triggerError = '';
+
+    public function send($from, $to, $subject, $body)
+    {
+        if (is_array($from) && count($from) == 2) {
+            $from = $from[0] . ' <' . $from[1] . '>';
+        } else if (is_array($from)) {
+            $from = implode('/', $from);
+        }
+
+        $reporting = error_reporting(0);
+
+        if ($this->triggerError) {
+            trigger_error($this->triggerError);
+            $result = false;
+        } else {
+			Logger::instance(static::$_logger)->log("Message from '$from' to '$to' with subject '$subject' and content\n$body");
+            $result = true;
+        }
+
+        error_reporting($reporting);
+
+        return $result;
+    }
+}
