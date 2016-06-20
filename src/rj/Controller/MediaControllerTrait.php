8614f718f2f68d59a20c367c3e12d2f536155595
hotfix <hotfix@localhost>
2016-06-20 15:13:26 +0300

    hotfix

--- src/rj/Controller/MediaControllerTrait.php
+++ src/rj/Controller/MediaControllerTrait.php
@@ -1,21 +1,22 @@
 <?php namespace Rj\Controller;
 
 use Phalcon\Mvc\Dispatcher;
-use Rj\Cache, Rj\FileInfo, Rj\Assert;
+use Rj\Cache, Rj\FileInfo, Rj\Assert, Rj\Helper;
 
 trait MediaControllerTrait {
 
 	protected function _getMediaDir() {
-		return DOCROOT . '/../app/media/';
+		return [ __DIR__ . '/../media/', DOCROOT . '/../app/media/' ];
 	}
 
 	public function getPackage($query) {
 		if (preg_match('#^/?([a-zA-Z0-9_-]+)/(.*)$#iD', $query, $pock)) {
-			$extList = [ '.zip', '.tar', '.gz' ];
-			foreach ($extList as $ext)
-				if (file_exists($pkgFileName = $this->_getMediaDir() . $pock[1] . $ext)) {
-					return realpath($pkgFileName);
-				}
+			$extList = [ '.zip' ];
+			foreach ($this->_getMediaDir() as $dir)
+				foreach ($extList as $ext)
+					if (file_exists($pkgFileName = $dir . $pock[1] . $ext)) {
+						return realpath($pkgFileName);
+					}
 		}
 	}
 
@@ -24,26 +25,26 @@ trait MediaControllerTrait {
 		Assert::found($query = $this->dispatcher->getParam('query'));
 		Assert::found($pkg = $this->getPackage($query));
 
-		$data = Cache::get('media-' . sha1($query), null, function() use ($pkg, $query) {
-			Assert::found($f = fopen('zip://' . $pkg . '#' . $query, 'r'));
-			$content = '';
-			while ( ! feof($f)) $content .= fread($f, 8 * 1024);
-			fclose($f);
+		$dst = DOCROOT . '/../public/media/';
+		@ Helper::mkdir($dst . dirname($query), 777);
 
-			$mime = null;
-			if ($ext = strtolower(pathinfo($query, PATHINFO_EXTENSION)))
-				$mime = empty(FileInfo::$types[$ext]) ? null : FileInfo::$types[$ext];
+		Assert::found($f = fopen('zip://' . $pkg . '#' . $query, 'r'));
+		Assert::true ($d = fopen($dst . $query, 'w'));
+		$content = '';
+		while ( ! feof($f)) {
+			$content .= ($data = fread($f, 8 * 1024));
+			fwrite($d, $data);
+		}
+		fclose($f);
 
-			return [
-				'mime'    => $mime,
-			    'content' => $content,
-			];
-		});
+		$mime = null;
+		if ($ext = strtolower(pathinfo($query, PATHINFO_EXTENSION)))
+			$mime = empty(FileInfo::$types[$ext]) ? null : FileInfo::$types[$ext];
 
-		if ( ! empty($data['mime']))
-			$this->response->setHeader('Content-Type', $data['mime']);
+		if ($mime)
+			$this->response->setHeader('Content-Type', $mime);
 
-		return $this->response->setContent($data['content']);
+		return $this->response->setContent($content);
 	}
 
 }
